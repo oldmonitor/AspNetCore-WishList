0c508371c229fb6d448a9cd4a33150555a7c4b29
oldmonitor <oldmonitor@users.noreply.github.com>
2018-07-28 15:23:02 -0400

    add item model and entity framework support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Item> items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
